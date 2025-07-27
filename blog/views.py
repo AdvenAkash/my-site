e4d535a7d679ca3a6b10acf8fee4619e2a3e4cac
Akash <advenakash1912@gmail.com>
2025-07-27 10:49:34 +0530

    Fetching data dyanamically from database

--- blog/views.py
+++ blog/views.py
@@ -1,65 +1,27 @@
-from django.shortcuts import render
-from datetime import date
+from django.shortcuts import render,get_object_or_404
+from .models import Post
 
-all_posts =[
-    {
-        "slug":"hike-in-the-moutains",
-        "image":"mountains.jpg",
-        "author":"Akash",
-        "date": date(2021,12,19),
-        "title":"Mountain Hiking",
-        "excerpt":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus istesaepe repellendus similique, enim vitae expedita sunt veritatis minusblanditiis",
-         "content":"""Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Necessitatibus similique earum iusto tempora animi sed modi alias eum officia reiciendis ducimus voluptatum excepturi perferendis, 
-        sunt eligendi dolorum molestiae, eveniet perspiciatis facere doloremque. Sequi cupiditate suscipit, molestias inventore excepturi, 
-        aspernatur deserunt aliquam perspiciatis vel enim porro tenetur laborum ad voluptatum in.
-        """
-    },
-    {
-        "slug":"coding-is-fun",
-        "image":"coding.jpg",
-        "author":"Akash",
-        "date": date(2024,9,21),
-        "title":"Coding time",
-        "excerpt":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus istesaepe repellendus similique, enim vitae expedita sunt veritatis minusblanditiis",
-        "content":"""Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Necessitatibus similique earum iusto tempora animi sed modi alias eum officia reiciendis ducimus voluptatum excepturi perferendis, 
-        sunt eligendi dolorum molestiae, eveniet perspiciatis facere doloremque. Sequi cupiditate suscipit, molestias inventore excepturi, 
-        aspernatur deserunt aliquam perspiciatis vel enim porro tenetur laborum ad voluptatum in.
-        """
-    },
-    {
-        "slug":"in-the-woods",
-        "image":"woods.jpg",
-        "author":"Akash",
-        "date": date(2022,1,23),
-        "title":"Walking in the woods",
-        "excerpt":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus istesaepe repellendus similique, enim vitae expedita sunt veritatis minusblanditiis",
-        "content":"""Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Necessitatibus similique earum iusto tempora animi sed modi alias eum officia reiciendis ducimus voluptatum excepturi perferendis, 
-        sunt eligendi dolorum molestiae, eveniet perspiciatis facere doloremque. Sequi cupiditate suscipit, molestias inventore excepturi, 
-        aspernatur deserunt aliquam perspiciatis vel enim porro tenetur laborum ad voluptatum in.
-        """
-    }
-]
 def get_date(post):
     return post['date']
 # Create your views here.
 def starting_page(request):
-    sorted_posts = sorted(all_posts,key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts=Post.objects.all().order_by("-date")[:3]
     return render(request,"blog/index.html",{
         "posts":latest_posts,
 
     })
 
 def posts(request):
+    all_posts = Post.objects.all()
     return render(request,"blog/all-posts.html",{
         "all_posts":all_posts,
     })
 
 def post_detail(request,slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    # identified_post = next(post for post in all_posts if post['slug'] == slug)
+    identified_post = get_object_or_404(Post,slug=slug)
     return render(request,"blog/post-detail.html",{
-        "post":identified_post
+        "post":identified_post,
+        "post_tag":identified_post.tag.all(),
+
     })
