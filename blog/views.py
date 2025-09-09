ef2d947734d1291a7a013c4ba3e8894948d8452e
Akash <advenakash1912@gmail.com>
2025-09-09 21:52:28 +0530

    Added Comment Model and Displaying comment in the portal!!

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render,get_object_or_404
+from django.views.generic import ListView
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .forms import CommentForm
 from .models import Post
-from django.views.generic import ListView,DetailView
-
 # Create your views here.
 
 class StartingPageView(ListView):
@@ -21,11 +24,34 @@ class AllPostsViews(ListView):
     ordering = ["-date"]
     context_object_name = "all_posts"
 
-class SinglePostView(DetailView):
-    template_name = "blog/post-detail.html"
-    model = Post
+class SinglePostView(View):
+    def get(self,request,slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post":post,
+            "post_tag":post.tag.all(),
+            "comment_form":CommentForm(),
+            "comments":post.comments.all().order_by("-id"),
+        }
+        return render(request,"blog/post-detail.html",context)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["post_tag"] = self.object.tag.all()
-        return context
+
+    def post(self,request,slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()    
+                   
+            return HttpResponseRedirect(reverse("post-detail-page",args=[slug]))
+        
+        context = {
+            "post":post,
+            "post_tag":post.tag.all(),
+            "comment_form":comment_form,
+            "comments":post.comments.all().order_by("-id"),
+        }
+        return render(request,"blog/post-detail.html",context)
+
+
